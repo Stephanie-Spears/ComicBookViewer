67873b5547dc65c6d37939eab15f90dd0a8e8840
Stephanie Spears <stephanie.spears1@gmail.com>
2018-07-02 03:00:39 -0700

    add day of week conditional

--- ComicBookViewer/Controllers/ComicBooksController.cs
+++ ComicBookViewer/Controllers/ComicBooksController.cs
@@ -3,10 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using DayOfWeek = System.DayOfWeek;
 
 namespace ComicBookViewer.Controllers
 {
     public class ComicBooksController : Controller
     {
+        public ActionResult Detail()
+        {
+            if (DateTime.Today.DayOfWeek == DayOfWeek.Tuesday)
+            {
+                return Redirect("/");
+            }
+
+            return Content("Hello via ComicBookController");
+        }
     }
 }
